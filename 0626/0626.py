10eab3da1d242b4a0fa4084fdb0bc9cc76dd0ca0
haekyu <hkpark627@gmail.com>
2018-06-26 21:47:19 +0900

    0626

--- 0626/0626.py
+++ 0626/0626.py
@@ -1,47 +1,58 @@
-class node:
-    def __init__(self):
-        self.val = None
-        self.nxt = None
+def is_factor_of_20(n):
+    if 20 % n == 0:
+        return True
+    else:
+        return False
 
-    def get_val(self):
-        return self.val
 
-    def get_nxt(self):
-        return self.nxt
+filtered = filter(is_factor_of_20, range(1, 20))
+factors_of_20 = list(filtered)
+print(factors_of_20)
 
-    def set_val(self, newval):
-        self.val = newval
+# class node:
+#     def __init__(self):
+#         self.val = None
+#         self.nxt = None
 
-    def set_nxt(self, newnxt):
-        self.nxt = newnxt
+#     def get_val(self):
+#         return self.val
 
-class linked_list:
-    def __init__(self):
-        self.n = 0
-        self.head = None
+#     def get_nxt(self):
+#         return self.nxt
 
-    def get
+#     def set_val(self, newval):
+#         self.val = newval
 
-    def set
+#     def set_nxt(self, newnxt):
+#         self.nxt = newnxt
 
-    def length
+# class linked_list:
+#     def __init__(self):
+#         self.n = 0
+#         self.head = None
 
-    def indexing(self, i):
-        i 번 next 따라가시면 됨
-        curr = get_head()
-        for i ...
-            ????
-        curr == i번째 노드
-        리턴 curr
+#     def get
 
-    def insert(self, i, v):
-        i-1번째 노드 = indexing(self, i):
-        ???
-        ???
+#     def set
 
-    def append
+#     def length
 
-    def delete(self, i)
+#     def indexing(self, i):
+#         i 번 next 따라가시면 됨
+#         curr = get_head()
+#         for i ...
+#             ????
+#         curr == i번째 노드
+#         리턴 curr
+
+#     def insert(self, i, v):
+#         i-1번째 노드 = indexing(self, i):
+#         ???
+#         ???
+
+#     def append
+
+#     def delete(self, i)
 
 
 
